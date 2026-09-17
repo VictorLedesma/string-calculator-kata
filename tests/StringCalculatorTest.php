ff2a009fdac4c115d258352145fbcdbcf89ea836
Victor <victorle44@gmail.com>
2026-09-17 09:49:05 +0200

    [ref] refactor all test cases following AAA

--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -20,51 +20,96 @@ final class StringCalculatorTest extends TestCase
     #[Test]
     public function it_returns_zero_for_an_empty_string(): void
     {
-        $this->assertSame('0', $this->calculator->add(''));
+        $numbers = "";
+        $expected = "0";
+
+        $result = $this->calculator->add($numbers);
+
+        $this->assertSame($expected, $result);
     }
 
     #[Test]
     public function it_returns_the_same_number_for_a_single_number(): void
     {
-        $this->assertSame('1', $this->calculator->add('1'));
+        $numbers = "1";
+        $expected = "1";
+
+        $result = $this->calculator->add($numbers);
+
+        $this->assertSame($expected, $result);
+
     }
 
     #[Test]
     public function it_returns_the_sum_of_two_numbers(): void
     {
-        $this->assertSame('3', $this->calculator->add('1,2'));
+        $numbers = "1,2";
+        $expected = "3";
+
+        $result = $this->calculator->add($numbers);
+
+        $this->assertSame($expected, $result);
+
     }
 
     #[Test]
     public function it_returns_the_sum_of_an_arbitrary_amount_of_numbers(): void
     {
-        $this->assertSame('28', $this->calculator->add('1,2,3,4,5,6,7'));
+        $numbers = "1,2,3,4,5,6,7";
+        $expected = "28";
+
+        $result = $this->calculator->add($numbers);
+
+        $this->assertSame($expected, $result);
+
     }
 
     #[Test]
     public function it_returns_the_sum_for_numbers_separated_by_line_breaks(): void
     {
-        $this->assertSame('6', $this->calculator->add("1\n2,3"));
+        $numbers = "1\n2,3";
+        $expected = "6";
+
+        $result = $this->calculator->add($numbers);
+
+        $this->assertSame($expected, $result);
+
     }
 
     #[Test]
     public function it_returns_error_when_line_break_follows_a_comma(): void
     {
-        $this->assertSame('Error: Invalid input', $this->calculator->add("1,\n"));
+        $numbers = "1,\n";
+        $expected = "Error: Invalid input";
+
+        $result = $this->calculator->add($numbers);
+
+        $this->assertSame($expected, $result);
+
     }
 
     #[Test]
     public function it_can_not_finish_with_a_separator(): void
     {
-        $this->assertSame('Error: Invalid input', $this->calculator->add("1,2,"));
+        $numbers = "1,2,";
+        $expected = "Error: Invalid input";
+
+        $result = $this->calculator->add($numbers);
+
+        $this->assertSame($expected, $result);
+
     }
 
     #[Test]
     public function it_allows_custom_separators(): void
     {
-        $calculator = new StringCalculator();
+        $numbers = "//;\n1;2";
+        $expected = "3";
 
-        $this->assertSame("3", $calculator->add("//;\n1;2"));
+        $result = $this->calculator->add($numbers);
+
+        $this->assertSame($expected, $result);
+
     }
 
 }
