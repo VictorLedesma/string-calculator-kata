db952188d2e3a48880efcd26f995d7d1ec240864
Victor <victorle44@gmail.com>
2026-09-18 08:35:53 +0200

    [green] error when line break follows a coma

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -39,6 +39,8 @@ final class StringCalculator
                 return '0';
             }
 
+            $this->validateSeparators($numbers);
+
             $numbers = $this->normalizeSeparators($numbers);
 
             $parts = $this->splitNumbers($numbers);
