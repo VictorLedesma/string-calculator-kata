3618caad5c56765e822fe4738ecf3b32cbe24383
Victor <victorle44@gmail.com>
2026-09-18 11:35:31 +0200

    [green] returns error divides by 0, evaluator case

--- src/ExpressionEvaluator.php
+++ src/ExpressionEvaluator.php
@@ -4,10 +4,22 @@ declare(strict_types=1);
 
 namespace MRC\StringCalculator;
 
+use InvalidArgumentException;
+
 final class ExpressionEvaluator
 {
     public function evaluate(string $expression): string
     {
-        return (string) eval ("return $expression;");
+        try {
+
+            if (str_contains($expression, '/0')) {
+                throw new InvalidArgumentException('Division by zero not allowed');
+            }
+
+            return (string) eval ("return $expression;");
+
+        } catch (InvalidArgumentException $exception) {
+            return $exception->getMessage();
+        }
     }
 }
