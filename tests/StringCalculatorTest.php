fb5aeab4db9ae6a15684f8c5bab38f3f8d7f31aa
Victor <victorle44@gmail.com>
2026-09-18 08:42:58 +0200

    [red] multiple errors in order, multiply case

--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -289,4 +289,16 @@ final class StringCalculatorTest extends TestCase
 
     }
 
+    #[Test]
+    public function it_returns_multiple_errors_in_order_mutiply_case(): void
+    {
+        $numbers = "-1,,-2";
+        $expected = "Negative not allowed: -1\nNumber expected but ',' found at position 3\nNegative not allowed: -2";
+
+        $result = $this->calculator->multiply($numbers);
+
+        $this->assertSame($expected, $result);
+
+    }
+
 }
