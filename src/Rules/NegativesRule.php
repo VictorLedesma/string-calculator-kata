10f2aafa62ba7f7344883e91bfab8ee7ddafed1a
Victor <victorle44@gmail.com>
2026-09-21 16:05:07 +0200

    [green] negatves not allowed rule

--- src/Rules/NegativesRule.php
+++ src/Rules/NegativesRule.php
@@ -6,10 +6,19 @@ namespace MRC\StringCalculator\Rules;
 
 use MRC\StringCalculator\Rule;
 
+
 final class NegativesRule implements Rule
 {
     public function validate(string $input): array
     {
-        return [];
+        $errors = [];
+        $parts = explode(',', $input);
+
+        foreach ($parts as $part) {
+            if ((float) $part < 0) {
+                $errors[] = 'Negatives not allowed';
+            }
+        }
+        return $errors;
     }
 }
