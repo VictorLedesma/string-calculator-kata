a6a3bd5954fa8739698e7e5b651b715b9c5604c4
Victor <victorle44@gmail.com>
2026-09-22 08:46:46 +0200

    [green] return multiple errors in order

--- src/Rules/MultipleErrorsRule.php
+++ src/Rules/MultipleErrorsRule.php
@@ -11,7 +11,23 @@ final class MultipleErrorsRule implements Rule
 {
     public function validate(string $input): array
     {
-        return [];
+        $errors = [];
+        $parts = explode(',', $input);
+        $position = 0;
 
+        foreach ($parts as $part) {
+            if ((float) $part < 0) {
+                $errors[] = 'Negative not allowed: ' . $part;
+            }
+
+            if ($part === '') {
+                $errors[] = "Error: Invalid input";
+
+            }
+
+            $position += strlen($part) + 1;
+
+        }
+        return $errors;
     }
 }
