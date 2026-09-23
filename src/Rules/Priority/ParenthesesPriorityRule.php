5550a11ddd1e56320b76adf40ec7cdd72e11fb0d
Victor <victorle44@gmail.com>
2026-09-23 16:12:26 +0200

    [green] it resolve different expressions

--- src/Rules/Priority/ParenthesesPriorityRule.php
+++ src/Rules/Priority/ParenthesesPriorityRule.php
@@ -9,11 +9,17 @@ use MRC\StringCalculator\ExpressionEvaluator;
 
 final class ParenthesesPriorityRule implements PriorityRules
 {
-
     public function apply(array $parts): array
     {
+        $number1 = (int) $parts[1];
+        $operator = $parts[2];
+        $number2 = (int) $parts[3];
 
+        $result = match ($operator) { '+' => $number1 + $number2};
 
-        return ['5', '*', '4'];
+        return [
+            (string) $result,
+            ...array_slice($parts, 5),
+        ];
     }
 }
