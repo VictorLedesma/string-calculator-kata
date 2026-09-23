9c8e455e497b67d19a0294f917f05010c8157077
Victor <victorle44@gmail.com>
2026-09-23 16:22:07 +0200

    [red] it resolve addition

---
+++ src/Rules/Priority/AddSubtractPriorityRule.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MRC\StringCalculator\Rules\Priority;
+
+use MRC\StringCalculator\PriorityRules;
+
+
+final class AddSubtractPriorityRule implements PriorityRules
+{
+    public function apply(array $parts): array
+    {
+        return [];
+
+    }
+}
---
+++ tests/AddSubtractPriorityRuleTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MRC\StringCalculator\Test;
+
+
+use MRC\StringCalculator\Rules\Priority\AddSubtractPriorityRule;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class AddSubtractPriorityRuleTest extends TestCase
+{
+    private AddSubtractPriorityRule $rule;
+
+    protected function setUp(): void
+    {
+        $this->rule = new AddSubtractPriorityRule();
+    }
+
+    #[Test]
+    public function it_resolves_addition_priority(): void
+    {
+        $parts = [
+            '4',
+            '+',
+            '2',
+        ];
+        $expected = [
+            '6',
+        ];
+
+        $result = $this->rule->apply($parts);
+
+        $this->assertSame($expected, $result);
+    }
+
+}
