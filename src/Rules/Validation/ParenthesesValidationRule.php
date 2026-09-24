e44d23157ea103fb9ecd139ae69c84e2842e5859
Victor <victorle44@gmail.com>
2026-09-24 14:31:40 +0200

    [green] it detects no operators following a parenthese reverse case

--- src/Rules/Validation/ParenthesesValidationRule.php
+++ src/Rules/Validation/ParenthesesValidationRule.php
@@ -22,6 +22,10 @@ final class ParenthesesValidationRule implements Rule
             return ['Invalid parentheses'];
         }
 
+        if (preg_match('/\)\d/', $input)) {
+            return ['Invalid parentheses'];
+        }
+
         return [];
     }
 
