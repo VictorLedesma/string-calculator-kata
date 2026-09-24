f3ee2feb2dfbfa546328058da80754df8a8d363c
Victor <victorle44@gmail.com>
2026-09-24 14:11:10 +0200

    [red] It detects empty parentheses

--- src/Rules/Validation/ParenthesesValidationRule.php
+++ src/Rules/Validation/ParenthesesValidationRule.php
@@ -6,14 +6,10 @@ namespace MRC\StringCalculator\Rules\Validation;
 
 use MRC\StringCalculator\Rule;
 
-final class ConsecutiveOperatorsRule implements Rule
+final class ParenthesesValidationRule implements Rule
 {
     public function validate(string $input): array
     {
-        if (preg_match('/[+\-*\/]{2,}/', $input)) {
-            return ['Consecutive operators are not allowed',];
-        }
-
         return [];
     }
 }
---
+++ tests/ParenthesesValidationRuleTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MRC\StringCalculator\Test;
+
+use MRC\StringCalculator\Rules\Validation\ConsecutiveOperatorsRule;
+use MRC\StringCalculator\Rules\Validation\ParenthesesValidationRule;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class ParenthesesValidationRuleTest extends TestCase
+{
+    private ParenthesesValidationRule $rule;
+
+    protected function setUp(): void
+    {
+        $this->rule = new ParenthesesValidationRule();
+    }
+
+    #[Test]
+    public function it_detects_empty_parentheses(): void
+    {
+        // Arrange
+        $input = '()';
+
+        $expected = [
+            'Invalid parentheses',
+        ];
+
+        // Act
+        $result = $this->rule->validate($input);
+
+        // Assert
+        $this->assertSame($expected, $result);
+    }
+
+}
