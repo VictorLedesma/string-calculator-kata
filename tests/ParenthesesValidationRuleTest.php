a34e7fc49b90ab1e64badc6587cbf92adc88cb5c
Victor <victorle44@gmail.com>
2026-09-24 14:20:50 +0200

    [red] it detects uncompleted parentheses

--- tests/ParenthesesValidationRuleTest.php
+++ tests/ParenthesesValidationRuleTest.php
@@ -34,4 +34,20 @@ final class ParenthesesValidationRuleTest extends TestCase
         $this->assertSame($expected, $result);
     }
 
+    #[Test]
+    public function it_detect_uncompleted_parentheses(): void
+    {
+        // Arrange
+        $input = '(2*3)+4)';
+
+        $expected = [
+            'Invalid parentheses',
+        ];
+
+        // Act
+        $result = $this->rule->validate($input);
+
+        $this->assertSame($expected, $result);
+    }
+
 }
