07d26b3fdd4a84f4bb69a6d7d294798621ccaf75
Victor <victorle44@gmail.com>
2026-09-24 08:38:13 +0200

    [green] it prioritizes multiplication over addition

--- src/Rules/Priority/ExpressionPriorityRule.php
+++ src/Rules/Priority/ExpressionPriorityRule.php
@@ -11,6 +11,13 @@ use MRC\StringCalculator\Rules\Operations\MultiplyRule;
 use MRC\StringCalculator\Rules\Operations\SubtractRule;
 
 
+
+// 1. Buscar la siguiente operación según prioridad
+// 2. Obtener left, operator y right
+// 3. Elegir la OperationRule correspondiente
+// 4. Calcular left y right
+// 5. Sustituir los 3 elementos por el resultado
+// 6. Repetir hasta reducir la expresión
 class ExpressionPriorityRule implements PriorityRules
 {
     /*public function __construct(
@@ -23,23 +30,18 @@ class ExpressionPriorityRule implements PriorityRules
 
     public function apply(array $parts): array
     {
-        // 1. Buscar la siguiente operación según prioridad
-        // 2. Obtener left, operator y right
-        // 3. Elegir la OperationRule correspondiente
-        // 4. Calcular left y right
-        // 5. Sustituir los 3 elementos por el resultado
-        // 6. Repetir hasta reducir la expresión
-        foreach ($parts as $index => $part) {
 
-            if (!in_array($part, ['+', '-', '*', '/'], true)) {
-                continue;
+        foreach (['*', '/', '+', '-'] as $operator) {
+
+            $index = array_search($operator, $parts, true);
+
+            if ($index !== false) {
+                return [
+                    $parts[$index - 1],
+                    $parts[$index],
+                    $parts[$index + 1],
+                ];
             }
-
-            return [
-                $parts[$index - 1],
-                $part,
-                $parts[$index + 1],
-            ];
         }
 
         return $parts;
