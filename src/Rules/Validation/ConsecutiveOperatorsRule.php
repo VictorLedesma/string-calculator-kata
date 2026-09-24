5eb01e5259df630a3edbdb1d545a5fe146cbc3f6
Victor <victorle44@gmail.com>
2026-09-24 12:54:54 +0200

    [red] it detects consecutive operators

---
+++ src/Rules/Validation/ConsecutiveOperatorsRule.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MRC\StringCalculator\Rules\Validation;
+
+use MRC\StringCalculator\Rule;
+
+final class ConsecutiveOperatorsRule implements Rule
+{
+    public function validate(string $input): array
+    {
+        return [];
+    }
+}
---
+++ tests/ConsecutiveOperatorsRuleTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MRC\StringCalculator\Test;
+
+use MRC\StringCalculator\Rules\Validation\ConsecutiveOperatorsRule;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class ConsecutiveOperatorsRuleTest extends TestCase
+{
+    private ConsecutiveOperatorsRule $rule;
+
+    protected function setUp(): void
+    {
+        $this->rule = new ConsecutiveOperatorsRule();
+    }
+
+    #[Test]
+    public function it_detects_consecutive_operators(): void
+    {
+        // Arrange
+        $input = '5*-4';
+
+        $expected = [
+            'Consecutive operators are not allowed',
+        ];
+
+        $result = $this->rule->validate($input);
+
+        $this->assertSame($expected, $result);
+    }
+}
